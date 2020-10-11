3dc9b5cde2862a2e8c0b7941c4de3ed879ed1b07
AramYako <Aramyako@hotmail.com>
2020-10-12 00:08:23 +0200

    Init shallow/deep clone project

---
+++ CloneObjectLibrary/CloneObjectLibrary/CloneClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CloneObjectLibrary
+{
+    public class CloneClass : ICloneClass
+    {
+        public T DeepClone<T>(T source)
+        {
+            XmlSerializer xmlSerilizer = new XmlSerializer(typeof(T));
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerilizer.Serialize(textWriter, source);
+
+                using (TextReader tr = new StringReader(textWriter.ToString()))
+
+                    return (T)xmlSerilizer.Deserialize(tr);
+            }
+        }
+
+        public T ShallowClone<T>(T source)
+        {
+            var clone = FormatterServices.GetUninitializedObject(source.GetType());
+            for (var type = source.GetType(); type != null; type = type.BaseType)
+            {
+                var fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                foreach (var field in fields)
+                    field.SetValue(clone, field.GetValue(source));
+            }
+            return (T)clone;
+
+        }
+    }
+}
---
+++ CloneObjectLibrary/CloneObjectLibrary/ICloneClass.cs
@@ -0,0 +1,8 @@
+﻿namespace CloneObjectLibrary
+{
+    public interface ICloneClass
+    {
+        T DeepClone<T>(T source);
+        T ShallowClone<T>(T source);
+    }
+}
